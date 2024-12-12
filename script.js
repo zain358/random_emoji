677a644e09892a41b0de98999072128b5a26a27b
APPROX Coding <chzain0319@gmail.com>
2024-12-12 19:00:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const btnE1 = document.getElementById("btn");
+const emojiNameE1 = document.getElementById("emoji-name");
+
+const emoji = [];
+
+
+async function getEmoji() {
+    let response = await fetch(
+      "https://emoji-api.com/emojis?access_key=773b58f681fb786fafdb8392e8b8a75ddc177fd1"
+    );
+
+    data = await response.json();
+
+    for(let i =0; i < 1500; i++){
+        emoji.push({
+            emojiName: data[i].character,
+            emojiCode: data[i].unicodeName,
+        });
+    }
+}
+
+getEmoji();
+
+
+btnE1.addEventListener("click",() =>{
+    const randomNum = Math.floor(Math.random() * emoji.length);
+    btnE1.innerText = emoji[randomNum].emojiName;
+    emojiNameE1.innerText = emoji[randomNum].emojiCode;
+});
